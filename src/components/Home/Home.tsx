19359415a1caff1a18786b35fc638b9b4bd06b8f
ngduc <ducjava@gmail.com>
2019-08-31 00:02:48 -0700

    feat: add whatwg-fetch, Req, json-server, faker;

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -3,11 +3,18 @@ import { view } from 'react-easy-state'
 import todoStore from 'stores/todoStore'
 
 export default view(() => {
-  const { items, create, remove } = todoStore
+  const { items, create, remove, fetchItems } = todoStore
   return (
     <div>
       <div>Home Page</div>
-      <button onClick={create}>＋ Add Item</button>
+
+      <button data-fetch onClick={fetchItems}>
+        fetchItems
+      </button>
+      <button data-add onClick={create}>
+        ＋ Add Item
+      </button>
+
       <ul>
         {items.map((item, idx) => (
           <li key={idx}>
